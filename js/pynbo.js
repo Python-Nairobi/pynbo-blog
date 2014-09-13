42938c1f31ad017d0e552761c99c8513402e3b18
James Nzomo <kazikubwa@gmail.com>
2014-09-13 19:27:21 +0300

    Update documentation

---
+++ js/pynbo.js
@@ -0,0 +1,6 @@
+//Skulpt
+$.getScript( "skulpt/codemirrorepl.js" )
+$.getScript( "skulpt/repl.js" )
+$.getScript( "skulpt/python.js" )
+$.getScript( "skulpt/skulpt.js" )
+$.getScript( "skulpt/skulpt-stdlib.js" )
